4989bcb788e172a2366f7905842cba2c7e342a0c
amparolazar557-creator <amparolazar557@gmail.com>
2026-09-14 14:36:34 +0800

    chore: publish ppte

---
+++ ppt/main.js
@@ -0,0 +1,13 @@
+// Keep Presentation callback registration synchronous for older WPS builds.
+document.write("<script type='text/javascript' src='js/symbols.js'></script>");
+document.write("<script type='text/javascript' src='js/ppt-api.js'></script>");
+document.write("<script type='text/javascript' src='js/taskpane.js'></script>");
+document.write("<script type='text/javascript' src='js/vendor/tweetnacl-fast.min.js'></script>");
+document.write("<script type='text/javascript' src='js/license-public-key.js'></script>");
+document.write("<script type='text/javascript' src='js/license.js'></script>");
+document.write("<script type='text/javascript' src='js/plot-config.js'></script>");
+document.write("<script type='text/javascript' src='js/plot-analysis.js'></script>");
+document.write("<script type='text/javascript' src='js/plotter.js'></script>");
+document.write("<script type='text/javascript' src='js/function-plot-document.js'></script>");
+document.write("<script type='text/javascript' src='js/function-plot-native.js'></script>");
+document.write("<script type='text/javascript' src='js/ribbon-ppt.js'></script>");
